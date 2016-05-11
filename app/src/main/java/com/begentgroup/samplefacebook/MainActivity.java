084f7eb5fe8557089e12da3f0b7ba372d5e10636
dongja94 <dongja94@gmail.com>
2016-05-11 09:23:38 +0900

    custom login button

--- app/src/main/java/com/begentgroup/samplefacebook/MainActivity.java
+++ app/src/main/java/com/begentgroup/samplefacebook/MainActivity.java
@@ -3,16 +3,25 @@ package com.begentgroup.samplefacebook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
+    LoginManager mLoginManager;
+    AccessTokenTracker mTracker;
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +29,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         callbackManager = CallbackManager.Factory.create();
-        LoginButton btn = (LoginButton)findViewById(R.id.login_button);
-        btn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        mLoginManager = LoginManager.getInstance();
+//        LoginButton btn = (LoginButton)findViewById(R.id.login_button);
+//        btn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//
+//            }
+//        });
+
+        loginButton = (Button)findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
+            public void onClick(View v) {
+                if (!isLogin()) {
+                    mLoginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                        @Override
+                        public void onSuccess(LoginResult loginResult) {
+                            Toast.makeText(MainActivity.this, "login success", Toast.LENGTH_SHORT).show();
+                        }
 
-            }
+                        @Override
+                        public void onCancel() {
+                            Toast.makeText(MainActivity.this, "login cancel", Toast.LENGTH_SHORT).show();
+                        }
 
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
+                        @Override
+                        public void onError(FacebookException error) {
+                            Toast.makeText(MainActivity.this, "login fail", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    mLoginManager.logInWithReadPermissions(MainActivity.this, null);
+                } else {
+                    mLoginManager.logOut();
+                }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mTracker == null) {
+            mTracker = new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                    changeButtonText();
+                }
+            };
+        } else {
+            mTracker.startTracking();
+        }
+        changeButtonText();
+    }
+
+    private void changeButtonText() {
+        if (isLogin()) {
+            loginButton.setText("logout");
+        } else {
+            loginButton.setText("login");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mTracker.stopTracking();
+    }
+
+    private boolean isLogin() {
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        return token!=null;
     }
 
     @Override
